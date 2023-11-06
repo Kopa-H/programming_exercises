a1b8e7f7479454dec8fc537f092dfec1538dd1e9
Kopa_From_PC <paucullaloren@gmail.com>
2023-11-06 23:16:48 +0100

    The algorithm is nearly finished.
    It still lacks of optimization or treatment of exceptions

--- 21_[Maximización del día más óptimo para robar].py
+++ 21_[Maximización del día más óptimo para robar].py
@@ -8,41 +8,48 @@ import time
 start_time = time.time()
 
 class Solution(object):
-    def check_if_there_is_time_days_before_and_after(self, i_day, time, security):
+    def check_first_condition(self, i_day, time, security):
         remaining_days_before =  i_day
         remaining_days_after = (len(security) - i_day) - 1
 
-        if ((time == 0) or (remaining_days_before - time) >= 0) and ((remaining_days_after - time) >= 0):
+        if ((remaining_days_before - time >= 0) and (remaining_days_after - time >= 0)):
             return True
 
-    def check_if_number_of_guards_time_days_before_are_non_increasing(self, i_day, time, security):
+    def check_second_and_third_condition(self, i_day, time, security):
         for t in range(time):
+            # Second condition
             if security[i_day - (time - t)] < security[i_day - (time - t - 1)]:
-                return False
-        return True
-
-    def check_if_number_of_guards_time_days_after_are_non_decreasing(self, i_day, time, security):
-        for t in range(time):
+                return
+            # Third condition
             if security[i_day + (time - t)] < security[i_day + (time - t - 1)]:
-                return False
+                return
         return True
 
     def goodDaysToRobBank(self, security, time):
-        self.listOfPotentialDays = []
+        potential_days = []
 
-        for i_day in range(len(security)):
-            first_condition = self.check_if_there_is_time_days_before_and_after(i_day, time, security)
-            if (first_condition):
-                second_condition = self.check_if_number_of_guards_time_days_before_are_non_increasing(i_day, time, security)
-                third_condition = self.check_if_number_of_guards_time_days_after_are_non_decreasing(i_day, time, security)
+        # If time is equal to zero:
+        if time == 0:
+            for i_day in range(len(security)):
+                potential_days.append(i_day)
 
-                if ((time==0) or second_condition and third_condition):
-                    self.listOfPotentialDays.append(i_day)
-                if len(security) > 1000 and time!=0 and len(set(security)) == 1:
-                    return self.listOfPotentialDays.append(security[0])
+        # If the array just have a given number:
+        elif len(set(security)) == 1:
+            for i_day in range(len(security)):
+                if self.check_first_condition(i_day, time, security):
+                    potential_days.append(i_day)
 
-        print(self.listOfPotentialDays)
-        return self.listOfPotentialDays
+        # For the rest of situations:
+        else:
+            for i_day in range(len(security)):
+                first_condition = self.check_first_condition(i_day, time, security)
+
+                if first_condition:
+                    if self.check_second_and_third_condition(i_day, time, security):
+                        potential_days.append(i_day)
+
+        print(potential_days)
+        return potential_days
 
 solution = Solution()
 solution.goodDaysToRobBank(security=[5,3,3,3,5,6,2], time=2)
